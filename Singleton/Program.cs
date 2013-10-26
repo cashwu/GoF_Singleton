a4e094941fb7cf05de119d706af61f5e50c6f5eb
cashwu <cash@cashwu.com>
2013-10-26 21:28:20 +0800

    FirstCommit

---
+++ Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public class FileSystem
+    {
+        private static FileSystem mFileSystem = new FileSystem();
+
+        private FileSystem()
+        {
+        }
+
+        public static FileSystem getInstance()
+        {
+            return mFileSystem;
+        }
+    }
+
+    public class FileSystem2
+    {
+        public static FileSystem2 current()
+        {
+            return new FileSystem2();
+        }
+    }
+}
---
+++ SingletonTests/FileSystemTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Singleton;
+
+namespace Singleton.Tests
+{
+    [TestClass()]
+    public class FileSystemTests
+    {
+        [TestMethod()]
+        public void getInstanceTest()
+        {
+            FileSystem file1 = FileSystem.getInstance();
+            Assert.IsNotNull(file1);
+
+            FileSystem file2 = FileSystem.getInstance();
+            Assert.IsNotNull(file2);
+
+            Assert.AreEqual(file1, file2);
+        }
+    }
+}
